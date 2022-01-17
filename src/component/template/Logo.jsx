a7d7a75da1d61b05d758e752a3dfb61a3c06dd23
pablo.bueno <pablo.bueno@khomp.com>
2022-01-17 09:51:51 -0300

    finalizacao

--- src/component/template/Logo.jsx
+++ src/component/template/Logo.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 import "./Logo.css";
-
-export default (props) => <aside className="logo">Logo</aside>;
+import logo from "../../assets/imgs/logo.png";
+import { Link } from "react-router-dom";
+// "http://files.cod3r.com.br/curso-web/logo.png"
+export default (props) => (
+  <aside className="logo">
+    <Link to="/" className="logo">
+      <img src={logo} alt="logo" />
+    </Link>
+  </aside>
+);
